4d18077cc33fb30b878aec89c400e793991e783b
satya <satyanandgupta845@gmail.com>
2025-11-24 22:45:17 +0530

    commit

--- src/OOPs/Final_Method.java
+++ src/OOPs/Final_Method.java
@@ -1,27 +1,27 @@
-package OOPs;
-
-class final2 {
-
-	final void get() { // can't override
-		System.out.println("Get1");
-	}
-
-}
-
-class final3 extends final2 {
-
-	void get() {
-		System.out.println("Get2");
-	}
-
-}
-
-public class Final_Method {
-
-	public static void main(String[] args) {
-
-		final3 t = new final3();
-		t.get();
-	}
-
-}
+//package OOPs;
+//
+//class final2 {
+//
+//	final void get() { // can't override
+//		System.out.println("Get1");
+//	}
+//
+//}
+//
+//class final3 extends final2 {
+//
+//	void get() {
+//		System.out.println("Get2");
+//	}
+//
+//}
+//
+//public class Final_Method {
+//
+//	public static void main(String[] args) {
+//
+//		final3 t = new final3();
+//		t.get();
+//	}
+//
+//}
